1f022acaf8118667f1950de092caea4c428e08c8
Gabriele Nakassima <gaby.naka@gmail.com>
2021-10-07 22:01:47 -0300

    usando o TDD na prática na criação do create car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,5 +1,3 @@
-import { inject, injectable } from "tsyringe";
-
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { AppError } from "@shared/errors/AppError";
@@ -13,13 +11,8 @@ interface IRequest {
   brand: string;
   category_id: string;
 }
-
-// @injectable()
 class CreateCarUseCase {
-  constructor(
-    // @inject("CarsRepository")
-    private carsRepository: ICarsRepository
-  ) {}
+  constructor(private carsRepository: ICarsRepository) {}
 
   async execute({
     name,
