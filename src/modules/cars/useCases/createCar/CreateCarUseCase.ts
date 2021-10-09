34fe447f45be3dbffe439829b2da1ebb384059d6
Gabriele Nakassima <gaby.naka@gmail.com>
2021-10-08 21:56:18 -0300

    finalizando a criação do create car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { AppError } from "@shared/errors/AppError";
@@ -11,8 +13,13 @@ interface IRequest {
   brand: string;
   category_id: string;
 }
+
+@injectable()
 class CreateCarUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
 
   async execute({
     name,
