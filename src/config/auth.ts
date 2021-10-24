4cfc686d2e7b3ad83fdf7e2d71b50e1671b4a975
Gabriele Nakassima <gaby.naka@gmail.com>
2021-10-24 20:23:46 -0300

    autenticação com refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "627620c243d021f8471489c1cc2abf35",
+  expires_in_token: "15m",
+  secret_refresh_token: "1c6e9de915255e85a169ff0a7a38f44f",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
